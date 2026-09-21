97276c60be87f02ae3913bbbbc7a080854c0004f
Samarth Gadekar <112534680+deathkernel@users.noreply.github.com>
2026-09-21 10:54:58 +0530

    Add dataset registry tests

---
+++ tests/test_text_dataset.py
@@ -0,0 +1,21 @@
+from src.text_dataset import DATASETS, describe_dataset, list_datasets
+
+
+def test_dataset_registry_contains_core_corpora():
+    assert {"tinystories", "wikitext2"} <= set(list_datasets())
+
+
+def test_dataset_description_is_copy():
+    description = describe_dataset("tinystories")
+    assert description["dataset_id"] == DATASETS["tinystories"]["dataset_id"]
+    description["description"] = "changed"
+    assert DATASETS["tinystories"]["description"] != "changed"
+
+
+def test_unknown_dataset_rejected():
+    try:
+        describe_dataset("does-not-exist")
+    except KeyError:
+        pass
+    else:
+        raise AssertionError("unknown datasets must raise KeyError")
