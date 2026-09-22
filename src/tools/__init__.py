3b4fc220a258f172908b7a07f9708c628d3aee0d
Samarth Gadekar <112534680+deathkernel@users.noreply.github.com>
2026-09-22 12:21:50 +0530

    Export Phase 16 tools

--- src/tools/__init__.py
+++ src/tools/__init__.py
@@ -1,5 +1,13 @@
 """Controlled host tools for TARA."""
 
+from .app_tools import AppToolResult, ApplicationTools, BrowserTools
+from .controller import ToolController, ToolDecision, ToolExecution
 from .file_tools import FileToolError, FileToolResult, FileTools
+from .input_tools import InputResult, InputTools
+from .terminal_tools import TerminalResult, TerminalTools
 
-__all__ = ["FileToolError", "FileToolResult", "FileTools"]
+__all__ = [
+    "AppToolResult", "ApplicationTools", "BrowserTools", "ToolController",
+    "ToolDecision", "ToolExecution", "FileToolError", "FileToolResult",
+    "FileTools", "InputResult", "InputTools", "TerminalResult", "TerminalTools",
+]
