af848bcc60cfb26ff3432d91466685369c5c6d21
Samarth Gadekar <112534680+deathkernel@users.noreply.github.com>
2026-09-24 17:24:39 +0530

    Expose workspace neuromodulation and active science

--- src/tara_mind/cognition/__init__.py
+++ src/tara_mind/cognition/__init__.py
@@ -3,6 +3,9 @@
 from .appraisal import AppraisalEngine, AppraisalInput, AppraisalState
 from .attention import AttentionItem, AttendedItem, SelectiveAttention
 from .executive import ActionCandidate, Decision, ExecutiveController, Goal
+from .global_workspace import GlobalWorkspace, WorkspaceItem
+from .neuromodulation import NeuromodulatoryController, NeuromodulatoryState
+from .active_experiment import ActiveExperimenter, ExperimentOption
 from .metacognition import MetacognitiveMonitor, MetacognitiveReport
 from .planning import ModelBasedPlanner, Plan
 from .reflection import Reflection, ReflectionEngine
